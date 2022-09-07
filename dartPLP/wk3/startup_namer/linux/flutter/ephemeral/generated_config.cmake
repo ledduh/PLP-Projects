56a85e178dbd59ddb1118e0b954ed76f26dca384
pop-job <pop-job@pop-os.localdomain>
2022-09-07 13:12:01 +0300

    html/css files

---
+++ dartPLP/wk3/startup_namer/linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,15 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/pop-job/Desktop/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/pop-job/Desktop/flutter/flutter/startup_namer" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/pop-job/Desktop/flutter"
+  "PROJECT_DIR=/home/pop-job/Desktop/flutter/flutter/startup_namer"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/pop-job/Desktop/flutter/flutter/startup_namer/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/pop-job/Desktop/flutter/flutter/startup_namer/lib/main.dart"
+)
